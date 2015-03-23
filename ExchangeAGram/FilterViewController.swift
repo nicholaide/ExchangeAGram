cf020f20f1a39c49bd0b6e8678337555f833cd40
Nicholai de Guzman <nicholaideguzman@nisnic-imac.dhcp.swmed.org>
2015-03-23 11:22:50 -0500

    intro to Grand Central Dispatch (GCD)

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -12,6 +12,9 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
 
     var thisFeedItem: FeedItem! //unwrap, so this var won't be an optional
     var collectionView: UICollectionView!
+    let kIntensity = 0.7
+    var context:CIContext = CIContext(options: nil)
+    var filters:[CIFilter] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,10 +29,9 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         collectionView.backgroundColor = UIColor.whiteColor()
         collectionView.registerClass(FilterCell.self, forCellWithReuseIdentifier: "MyCell")
         
-        
-        
         self.view.addSubview(collectionView)
         
+        filters = photoFilters()
         
     }
     
@@ -38,8 +40,10 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         // Dispose of any resources that can be recreated.
     }
     
+    //UI CollectionViewDataSource
+    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return self.filters.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -47,8 +51,69 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         let cell:FilterCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell", forIndexPath: indexPath) as FilterCell
         
         cell.imageView.image = UIImage(named: "Placeholder")
+        
+        //Note: do UI changes on main thread, not on background threads
+        
+        let filterQueue:dispatch_queue_t = dispatch_queue_create("filter queue", nil)
+        dispatch_async(filterQueue, { () -> Void in
+            let filterImage = self.filteredImageFromImage(self.thisFeedItem.image, filter: self.filters[indexPath.row])
+            //the thread is done at this point
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                cell.imageView.image = filterImage
+            })
+        })
         return cell
         
+    }
+    
+    //Helpers (photofilters)
+    
+    func photoFilters () -> [CIFilter] {
+        let blur = CIFilter(name: "CIGaussianBlur")
+        let instant = CIFilter(name: "CIPhotoEffectInstant")
+        let noir = CIFilter(name: "CIPhotoEffectNoir")
+        let transfer = CIFilter(name: "CIPhotoEffectTransfer")
+        let unsharpen = CIFilter(name: "CIUnsharpMask")
+        let monochrome = CIFilter(name: "CIColorMonochrome")
+        
+        let colorControls = CIFilter(name: "CIColorControls")
+        colorControls.setValue(0.5, forKey: kCIInputSaturationKey)
+        
+        let sepia = CIFilter(name: "CISepiaTone")
+        sepia.setValue(kIntensity, forKey: kCIInputIntensityKey)
+        
+        let colorClamp = CIFilter(name: "CIColorClamp")
+        colorClamp.setValue(CIVector(x: 0.9, y: 0.9, z: 0.9, w: 0.9), forKey: "inputMaxComponents")
+        colorClamp.setValue(CIVector(x: 0.2, y: 0.2, z: 0.2, w: 0.2), forKey: "inputMinComponents")
+        
+        //composite filters
+        let composite = CIFilter(name: "CIHardLightBlendMode")
+        composite.setValue(sepia.outputImage, forKey: kCIInputImageKey)
+        
+        let vignette = CIFilter(name: "CIVignette")
+        vignette.setValue(composite.outputImage, forKey: kCIInputImageKey)
+        
+        vignette.setValue(kIntensity * 2, forKey: kCIInputIntensityKey)
+        vignette.setValue(kIntensity * 30, forKey: kCIInputRadiusKey)
+        
+        return [blur, instant, noir, transfer, unsharpen, monochrome, colorControls, sepia, colorClamp, composite, vignette]
+    }
+    
+    
+    func filteredImageFromImage (imageData: NSData, filter: CIFilter) -> UIImage {
+        
+        let unfilteredImage = CIImage(data: imageData)
+        filter.setValue(unfilteredImage, forKey: kCIInputImageKey)
+        let filteredImage:CIImage = filter.outputImage
+        
+        //.extent determines the rect or dimensions of the image? vs doing it on the fly
+        let extent = filteredImage.extent()
+        
+        let cgImage:CGImageRef = context.createCGImage(filteredImage, fromRect: extent)
+        
+        let finalImage = UIImage(CGImage: cgImage)
+        
+        return finalImage!
     }
     
 
